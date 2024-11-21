5167899be7a3434e759718bbfb239b1e7998d4ec
Kunal Vats <1kunalvats9@gmail.com>
2024-11-22 00:25:53 +0530

    opening sites and searching for anything on google done

--- script.js
+++ script.js
@@ -20,7 +20,33 @@ recognition.onend = () =>{
 recognition.onresult = (event) =>{
     let current = event.resultIndex;
     let transcript = event.results[current][0].transcript;
+    transcript = transcript.toLowerCase();
     console.log(transcript);
+    if(transcript.includes("hi Jarvis")||transcript.includes("hello Jarvis")){
+        readOut("Hello, how are you?");
+        console.log("Hello Sir");
+    }
+    if(transcript.includes("open youtube")){
+        readOut("Opening Youtube");
+        window.open("https://www.youtube.com");
+    }
+    if(transcript.includes("open google")){
+        readOut("Opening Google");
+        window.open("https://www.google.com");
+    }
+    if(transcript.includes("open my github")){
+        readOut("Opening Your Github Profile");
+        window.open("https://github.com/1Kunalvats9");
+    }
+    if(transcript.includes("open my linkedin")){
+        readOut("Opening Your Linkedin Profile");
+        window.open("www.linkedin.com/in/kunal-vats-b67aa2309");
+    }
+    //google search
+    if(transcript.includes("search for")){
+        readOut("Here are the results for "+transcript.replace("search for", ""));
+        window.open("https://www.google.com/search?q="+transcript.replace("search for", ""));
+    }
 }
 
 startBtn.addEventListener("click", () =>{
@@ -42,6 +68,6 @@ function readOut(message){
 }
 
 //example of readOut
-speakBtn.addEventListener("click", () =>{
-    readOut("Kunal  4 baap ka");
-})
+// speakBtn.addEventListener("click", () =>{
+//     readOut("Kunal  4 baap ka");
+// })
